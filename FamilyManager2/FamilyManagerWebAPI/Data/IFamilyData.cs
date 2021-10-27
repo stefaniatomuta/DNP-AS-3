a26a8f6119564f46d71c0351f82d27c75c4d868b
realfoxsterdota <foxsterdota@gmail.com>
2021-10-27 15:49:08 +0200

    Add Child controller

--- FamilyManager2/FamilyManagerWebAPI/Data/IFamilyData.cs
+++ FamilyManager2/FamilyManagerWebAPI/Data/IFamilyData.cs
@@ -5,24 +5,19 @@ using Models;
 namespace FamilyManagerWebAPI.Data {
     public interface IFamilyData {
         
-        
         //Get
         Task<IList<Family>> GetFamiliesAsync();
         Task<IList<Person>> GetPeopleAsync();
         Task<Family> GetFamilyAsync(string streetName, int houseNumber);
         Task<Person> GetPersonByIdFirstLastNameAsync(int id, string firstname, string lastname);
         Task<Pet> GetPetAsync(int petId);
-        Task<IList<string>> GetEyeColorsAsync();
-        Task<IList<string>> GetHairColorsAsync();
         Task<IList<Adult>> GetAdultsAsync();
-        Task<IList<Child>> GetChildrenAsync();
         Task<IList<Pet>> GetPetsAsync();
         Task<Person> GetPersonAsync();
 
         //Add
         Task<Family> AddFamilyAsync(Family family);
         Task<Adult> AddAdultAsync(Adult adult, Family family);
-        Task<Child> AddChildAsync(Child child, Family family);
         Task<Pet> AddPetAsync(Pet pet, Family family);
         Task<Pet> AddPetAsync(Pet pet, Family family, Child child);
 
