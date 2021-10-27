9dfc5b0b01800ec34918bb12a1f981cd47f4e69e
stefaniatomuta <304173@via.dk>
2021-10-27 15:51:26 +0200

    IFamilyDAO

--- FamilyManager2/FamilyManagerWebAPI/Data/IFamilyData.cs
+++ FamilyManager2/FamilyManagerWebAPI/Data/IFamilyData.cs
@@ -14,14 +14,12 @@ namespace FamilyManagerWebAPI.Data {
         Task<Pet> GetPetAsync(int petId);
         Task<IList<string>> GetEyeColorsAsync();
         Task<IList<string>> GetHairColorsAsync();
-        Task<IList<Adult>> GetAdultsAsync();
         Task<IList<Child>> GetChildrenAsync();
         Task<IList<Pet>> GetPetsAsync();
         Task<Person> GetPersonAsync();
-
+        
         //Add
         Task<Family> AddFamilyAsync(Family family);
-        Task<Adult> AddAdultAsync(Adult adult, Family family);
         Task<Child> AddChildAsync(Child child, Family family);
         Task<Pet> AddPetAsync(Pet pet, Family family);
         Task<Pet> AddPetAsync(Pet pet, Family family, Child child);
