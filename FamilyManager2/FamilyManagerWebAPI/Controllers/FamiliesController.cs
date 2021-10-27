7db2b372ffc2ca8aaff30fd9edec8d6598fdbb4b
Sombrero <adriana@grecea.net>
2021-10-27 14:10:00 +0200

    family controller

---
+++ FamilyManager2/FamilyManagerWebAPI/Controllers/FamiliesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyManagerWebAPI.Controllers {
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class FamiliesController {
+        
+        
+        
+    }
+}
