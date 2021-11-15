c79a7e5e1dfa255e2b6fdbb39cfe248d414d1812
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2021-11-15 11:47:59 +0100

    Model

--- FamilyManager2/FamilyManagerClassLibrary/Models/User.cs
+++ FamilyManager2/FamilyManagerClassLibrary/Models/User.cs
@@ -1,5 +1,8 @@
-﻿namespace Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models {
     public class User {
+        [Key]
         public string Username { get; set; }
         public string Password { get; set; }
         public Role Role { get; set; }
